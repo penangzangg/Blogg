65e1f2f9d3d989e6b07a79d7e7bee68839818e80
penangzangg <67513720+penangzangg@users.noreply.github.com>
2022-04-12 16:37:35 +0700

    Create penangzang.js

---
+++ penangzang.js
@@ -0,0 +1,40 @@
+    var go_current     	= window.location.href;
+    var reff     	= document.referrer;
+    
+
+    function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+    var direct_link_ads = rChoice([
+                            "https://butterflypronounceditch.com/wm1ja4md4h?key=f00ab318503571edee782252de47bc10"
+            
+    var ars             = rChoice([
+                            "https://penangzang.my.id"
+                        ]);
+
+
+    var dir_type        = "arsae"; //refresh, domain, path, arsae
+
+
+    if(dir_type == 'refresh')
+    {
+        //REFRESH
+        console.log('refresh..');
+    }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
