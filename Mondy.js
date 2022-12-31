ec0378700d31f056e142b0e6ebd87afd8277d21c
penangzangg <67513720+penangzangg@users.noreply.github.com>
2023-01-01 01:05:14 +0700

    Update Mondy.js

--- Mondy.js
+++ Mondy.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : '069eab77382a5755a739f45b209abf06',
 		'format' : 'iframe',
 		'height' : 250,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://invaderimmenseimplication.com/069eab77382a5755a739f45b209abf06/invoke.js"></scr' + 'ipt>');
-</script>
